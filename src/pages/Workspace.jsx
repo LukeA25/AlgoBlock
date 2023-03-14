b916c1c13827bb0f783e21368a50f375b5a6a65e
LukeA25 <luke1chip@gmail.com>
2023-03-14 13:45:56 -0400

    Update Workspace.jsx

--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -3,8 +3,8 @@ function Workspace() {
       <div class="text-green-500 bg-black h-screen">
         <div class="w-24 h-screen bg-white">
           <ul>
-            <li class="text-center h-16 text-align-middle">Buy</li>
-            <li class="text-center h-16 align-middle">Sell</li>
+            <li>Buy</li>
+            <li>Sell</li>
           </ul>
         </div>
         <div class="bg-green-500"></div>
